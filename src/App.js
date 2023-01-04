bb1c8151196610b9356e7f4f2aad5c8a0f5eabca
adesha <adeshaprabhu@gmail.com>
2023-01-05 01:59:29 +0530

    basename changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { UserDetail } from "./UserDetail";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter  basename={window.location.pathname || ''>
         <Routes>
           <Route exact path="/" element={<Landing />}></Route>
           <Route path="/userDetail" element={<UserDetail />}></Route>
